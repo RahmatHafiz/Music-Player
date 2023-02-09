a3a3c4a89ff97b7199721c3c95b121d9b3c27b14
Rahmat Hafiz J <rahmat.hafiz.jayadiningrat@students.uin-suska.ac.id>
2023-02-10 00:01:15 +0700

    Initial commit

---
+++ daftarlagu.js
@@ -0,0 +1,38 @@
+let allMusic = [
+    {
+        name: "Kingslayer",
+        artist: "Bring Me The Horizon",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Thank You",
+        artist: "Dido",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Natural",
+        artist: "Imagine Dragons",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Saint Seiya",
+        artist: "Pegasus Fantasy",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "Last Resort",
+        artist: "Papa Roach",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "Look What You Made Me Do",
+        artist: "Taylor Swift",
+        img: "music-6",
+        src: "music-6"
+    },
+];
